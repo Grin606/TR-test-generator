8c972762a468fb788991a307ac2093ce2cd4860d
Maria Go <margilit@gmail.com>
2015-03-25 18:25:28 +0200

    cahnges in randFunc

--- TR-TestGeneration/src/tel_ran/tests/tools/RandFunc.java
+++ TR-TestGeneration/src/tel_ran/tests/tools/RandFunc.java
@@ -1,5 +1,6 @@
 package tel_ran.tests.tools;
 
+
 public class RandFunc {
 	
 	public static int IntRandomInRange(int nMin, int nMax) {
@@ -20,6 +21,20 @@ public class RandFunc {
 		if (Math.random() >= 0.5) return true;
 		else return false;
 	}
+	
+	public static int getRandomOfTwo(int first, int second) {
+		 double r = Math.random();
+		 if (r < 0.5) return first;
+		 else return second;
+	}
+	
+	public static int getRandomOfThree(int first, int second, int third) {
+		 double r = Math.random();
+		 if (r < 0.3333333) return first;
+		 if (r < 0.6666666) return second;
+		 return third;
+	}
+	
 	public static double DoubleRandomInRange (double a, double b) {
 		return a + Math.random()*(b-a);
 	}
