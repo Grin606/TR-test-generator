eb7ee21b61c155b8799260f0a058dd7c05dc3c6d
Maria Go <margilit@gmail.com>
2015-04-15 06:59:35 +0300

    Changes for Amir (+saving name of created folder in the file name)

--- TR-TestGeneration/src/tel_ran/tests/processor/TestProcessor.java
+++ TR-TestGeneration/src/tel_ran/tests/processor/TestProcessor.java
@@ -55,27 +55,29 @@ public class TestProcessor {
 
 	public List<String[]> processing (GetTaskGenerate taskGen, int number, String path, int maxLvl) throws Exception {
 		String dirName = taskGen.getDirName();		
-		dirName = path.concat(dirName);	
+		String newPath = path.concat(dirName);	
 		
-		if(!new File(dirName).exists() && !new File(dirName).mkdir())
-				System.out.println("Creating directory " + dirName + " failed");
-			else
-				path = dirName.concat(File.separator);		
-				
+		if(!new File(newPath).exists() && !new File(newPath).mkdir()) {
+				System.out.println("Creating directory " + newPath + " failed");
+				dirName = File.separator;
+		}
+			else {
+				path = newPath.concat(File.separator);
+				dirName = File.separator.concat(dirName).concat(File.separator);
+			}
+		
 		Testing_Problem testTask = null;
+		
 						long time1 = System.currentTimeMillis();
 			
 		BufferedImage res;	
 		String imgName;
 		String[] dsc;
 		int step, lvl = 1;
-		if (number % maxLvl == 0)
-			step = number/maxLvl;
-		else
-			step = number/maxLvl +1;		
+		step = number/maxLvl;
+		if (number % maxLvl != 0)
+			step++;	
 		int th = step;
-		
-
 		
 						long time2 = System.currentTimeMillis();
 						long time3, time4, time34 = 0L, time5, time54=0L, time6, time56 = 0L, time8, time85 = 0L, time86 = 0L;
@@ -101,13 +103,12 @@ public class TestProcessor {
 			File newImage = new File(path + imgName);
 			ImageIO.write(res, "jpeg", newImage);	
 			
-						time8 = System.currentTimeMillis();
-						
+						time8 = System.currentTimeMillis();				
 			
 			
 			dsc = new String[7];
 			dsc[0] = testTask.getDescription();
-			dsc[1] = imgName;
+			dsc[1] = dirName.concat(imgName);
 			dsc[2] = testTask.getName();
 			dsc[3] = Integer.toString(lvl);			
 			dsc[4] = testTask.getCorrectAnswerChar();			
@@ -129,20 +130,20 @@ public class TestProcessor {
 						long time71 = time7 - time1;
 						long time21 = time2 - time1;
 		
-//						System.out.println("Общее время исполнения - \t" + time71);
-//						System.out.println(" ");
-//						System.out.println("1. Генерация файлов и свич - \t" + time21);
-//						System.out.println("2. Генерация задач - \t\t" + time34);
-//						System.out.println("3. Обработка картинок - \t" + time54);
-//						System.out.println("   3.1. Калькуляция \t\t" + Image.time01);
-//						System.out.println("        3.1.1. расчет и ресайз \t" + Image.time03);
-//						System.out.println("        3.1.2. сеттинг координат \t" + Image.time04);
-//						System.out.println("   3.2. Рисование в буфере \t" + Image.time02);
-//						System.out.println("4. Заипсь в файлы - \t\t" + time56);
-//						System.out.println("   4.1. Картинки - \t\t" + time85);
-//						System.out.println("   4.2. Текст и список - \t" + time86);
-//						System.out.println(" ");
-//						
+						System.out.println("Общее время исполнения - \t" + time71);
+						System.out.println(" ");
+						System.out.println("1. Генерация файлов и свич - \t" + time21);
+						System.out.println("2. Генерация задач - \t\t" + time34);
+						System.out.println("3. Обработка картинок - \t" + time54);
+						System.out.println("   3.1. Калькуляция \t\t" + Image.time01);
+						System.out.println("        3.1.1. расчет и ресайз \t" + Image.time03);
+						System.out.println("        3.1.2. сеттинг координат \t" + Image.time04);
+						System.out.println("   3.2. Рисование в буфере \t" + Image.time02);
+						System.out.println("4. Заипсь в файлы - \t\t" + time56);
+						System.out.println("   4.1. Картинки - \t\t" + time85);
+						System.out.println("   4.2. Текст и список - \t" + time86);
+						System.out.println(" ");
+						
 		rep.displayList();
 		rep.displayInFile(path);
 						
